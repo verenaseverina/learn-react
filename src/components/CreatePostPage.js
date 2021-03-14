0c7d24cd41a9455433e936e47490e1dee392421a
Jeffrey Marcelino <guemarcelino123@email.com>
2021-03-14 19:53:42 +0700

    Create Post Page dan validate

---
+++ src/components/CreatePostPage.js
@@ -0,0 +1,77 @@
+import { useState } from 'react'
+import '../styles/TableStyleCreate.css'
+
+const CreatePostPage = () => {
+
+    const [author, setAuthor] = useState("");
+    const [post_description, setPost_Description] = useState("");
+    // const [category, setCategory] = useState("");
+
+    const[authorErr, setAuthorErr] = useState({});
+    const [post_descriptionErr, setPost_DescriptionErr] = useState({});
+    // const [categoryErr, setCategoryErr] = useState({});
+
+    const onSubmit = (e)=>{
+        e.preventDefault();
+        const isValid = formValidation();
+    }
+
+    const formValidation = () =>{
+        const authorErr = {};
+        const post_descriptionErr = {};
+        // const categoryErr = {};
+        let isValid = true;
+
+        if(author.trim().length <= 20){
+            authorErr.authorNameShort = "[ 20 Character ]" 
+            isValid = false;
+        }
+
+        if(post_description.trim().length <= 100){
+            post_descriptionErr.post_descriptionShort = "[ 100 Character]"
+            isValid = false;
+        }
+
+        setAuthorErr(authorErr);
+        setPost_DescriptionErr(post_descriptionErr);
+        return isValid;
+    }
+
+    return (
+        <div>
+            <form onSubmit={onSubmit} className='formCreate'>
+                <label>
+                    <input type="text" 
+                           name = "author"
+                           value={author} 
+                           placeholder='Author' 
+                           onChange={(e)=>{setAuthor(e.target.value)}}/>
+                    <br/>
+                    {Object.keys(authorErr).map((key) => {
+                        return  <div style = {{color : "red"}}>{authorErr[key]}</div>
+                    })}
+                    <br/>
+                    <input type="text" 
+                           name = "post_description"
+                           value={post_description} 
+                           placeholder='Post_Description'
+                           onChange={(e)=>{setPost_Description(e.target.value)}}/>
+                    <br/>
+                    {Object.keys(post_descriptionErr).map((key) => {
+                        return  <div style = {{color : "red"}}>{post_descriptionErr [key]}</div>
+                    })}
+                    <br/>
+                    {/* <input type="text" 
+                           value={category} 
+                           placeholder='Category'
+                           onChange={(e)=>{setCategory(e.target.value)}}/>
+                    <br/><br/> */}
+                </label>
+                <br/> <br/>
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    )
+}
+
+export default CreatePostPage
---
+++ src/components/ListPostPage.js
@@ -0,0 +1,56 @@
+import '../styles/TableStyleList.css'
+import {useState} from 'react'
+import DateTime from './DateTime'
+
+const ListPostPage = () => {
+    const [datas, setDatas] = useState([{
+        id: 1,
+        author: 'Jeffrey',
+        post_description: 'Learn React',
+        post_datetime: DateTime(),
+        category: 'Learning'
+    },
+    {
+        id: 2,
+        author: 'Ghozi',
+        post_description: 'Learn Vue',
+        post_datetime: DateTime(),
+        category: 'Learning and make website'
+    },
+    {
+        id: 3,
+        author: 'Garin',
+        post_description: 'Learn Angular',
+        post_datetime: DateTime(),
+        category: 'Learning'
+    }
+])
+
+    return (
+        <div>
+            <table className='List'>
+                <thead className='headTable'>
+                    <tr>
+                        <th >Id</th>
+                        <th>Author</th>
+                        <th>Post_Description</th>
+                        <th>Post_Datetime</th>
+                        <th>Category</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {datas.map((d) => 
+                    (<tr key={d.id}>
+                        <th>{d.id}</th>
+                        <th>{d.author}</th>
+                        <th>{d.post_description}</th>
+                        <th>{d.post_datetime}</th>
+                        <th>{d.category}</th>
+                    </tr>))}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default ListPostPage
